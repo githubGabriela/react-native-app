656dc067f85accca4f8cf6aa21a1488d37f4afc7
Gabriela <gabriela_balasel@yahoo.com>
2017-11-28 10:02:59 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>This app is working!!!</Text>
+          <Text>Happy girl!!!!</Text>
+          <Text>Testing</Text>
+          
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
